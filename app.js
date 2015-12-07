982531b955db2a316cde86bc6fabd1a92f16c458
kminevic <kminevich@strategicpartners.net>
2015-12-07 00:21:58 -0800

    initial commit

--- app.js
+++ app.js
@@ -1,13 +1,52 @@
-var http = require('http');
+/*
+	Problem: We need a simple way to look at a user's badge count and JavaScript points
+	Solution: Use node.js to connect to Treehouse's API to get profile information to print out
+ */
 
-var hostname = '127.0.0.1';
-var port = 1337;
+var http = require('http'),
+	https = require('https'),
+	url = "https://teamtreehouse.com/",
+	profileName = "konstantinminevich";
 
-http.createServer((req, res) => {
-	res.writeHead(200, {
-		'Content-Type': 'text/plain'
+// Print profile data
+function printMessage(username, badgeCount, points) {
+	var message = username + ' has ' + badgeCount + ' total badge(s) and ' + points + ' in JavaScript';
+	console.log(message);
+}
+// Print error messages
+function printError(error) {
+	console.log(error.message);
+}
+
+// 1. Connect to the API URL (http://teamtreehouse/profileName.json)
+var request = https.get(url + profileName + '.json', function(response) {
+	var body = '';
+	response.setEncoding('utf8');
+	// 2. Read the data
+	response.on('data', function(dataChunk) {
+		body += dataChunk;
 	});
-	res.end('Hello World\n');
-}).listen(port, hostname, () => {
-	console.log('Server running at http://' + hostname + ':' + port + '/');
+	// When data stream ends...
+	response.on('end', function() {
+		if (response.statusCode === 200){
+			// Parse errors
+			try {
+				// 3. Parse the data
+				var profile = JSON.parse(body);
+				// 4. Print the data out
+				printMessage(profile.name, profile.badges.length, profile.points.JavaScript);
+				console.log('------- end of data stream -------');
+			} catch(error) {
+				printError(error);
+			}
+		} else {
+			// status code error
+			printError({
+				message: 'There was a problem acquiring profile information for ' + profileName + '. Status code: ' + http.STATUS_CODES[response.statusCode]
+			});
+		}
+	});
 });
+
+// Connection errors
+request.on('error', printError);
