7c3e1d80ac9d528dcbab45c1837172841b590dfa
kminevic <kminevich@strategicpartners.net>
2015-12-06 17:46:11 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+var hostname = '127.0.0.1';
+var port = 1337;
+
+http.createServer((req, res) => {
+	res.writeHead(200, {
+		'Content-Type': 'text/plain'
+	});
+	res.end('Hello World\n');
+}).listen(port, hostname, () => {
+	console.log('Server running at http://' + hostname + ':' + port + '/');
+});
